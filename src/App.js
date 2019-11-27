2e40348e2eb6fdf9de70bc933a77802077f4a81b
BreezyP <jorge.a.garcia12@gmail.com>
2019-11-26 23:26:06 -0600

    rearanged components and implemented more css with animations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from "./Header";
 import './App.css'
 
 const App = () => {
     return (
-        <div className="header">
-            App
+        <div>
+            <Header/>
         </div>);
 };
 
